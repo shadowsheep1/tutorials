60b08e34156a416db5eee8acd9f7d5516c51e4ec
root <root@localhost.localdomain>
2017-05-10 20:51:39 +0200

    Makes minor changes for the minimal configuration. Readd dhcpd.con

--- rouge_ap/01_setup_eth0.sh
+++ rouge_ap/01_setup_eth0.sh
@@ -2,10 +2,10 @@
 
 #@author Fabio Bombardi
 
-airmon-ng check kill && \
-ifconfig eth0 up && \
-ifconfig eth0 192.168.1.2/24 && \
-route add default gw 192.168.1.1
+#airmon-ng check kill && \
+#ifconfig eth0 up && \
+#ifconfig eth0 192.168.1.2/24 && \
+#route add default gw 192.168.1.1
 
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
-ping www.versionestabile.it -c 
+ping www.versionestabile.it -c 3 
--- rouge_ap/02_setup_ap_wlan0.sh
+++ rouge_ap/02_setup_ap_wlan0.sh
@@ -2,5 +2,6 @@
 
 #@author Fabio Bombardi
 
+airmon-ng check kill && \
 airmon-ng start wlan0 && \
 airbase-ng --essid "free-hostspot" -c1 wlan0mon
